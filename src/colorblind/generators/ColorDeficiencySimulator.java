3452761e08a2c335158a5c798e9f84d36c553347
Jim <james.schmitz@gmail.com>
2016-07-08 14:26:31 -0400

    fix bug with default renderer in Processing 3. thankfully an easy fix!

--- src/colorblind/generators/ColorDeficiencySimulator.java
+++ src/colorblind/generators/ColorDeficiencySimulator.java
@@ -21,91 +21,95 @@ import colorblind.generators.util.Vector;
 
 public class ColorDeficiencySimulator extends ColorTranformGenerator {
 
-    public ColorDeficiencySimulator(Deficiency colorBlindness) {
-        super(colorBlindness);
-    }
+	public ColorDeficiencySimulator(Deficiency colorBlindness) {
+		super(colorBlindness);
+	}
 
-    public static ColorDeficiencySimulator createSimulator(Deficiency colorBlindness) {
-        switch (colorBlindness) {
-        case PROTANOPE:
-            return new ColorDeficiencySimulator(Deficiency.PROTANOPE);
-        case DEUTERANOPE:
-            return new ColorDeficiencySimulator(Deficiency.DEUTERANOPE);
-        case TRITANOPE:
-            return new ColorDeficiencySimulator(Deficiency.TRITANOPE);
-        case ACHROMATOPE:
-            return new ColorDeficiencySimulator(Deficiency.ACHROMATOPE);
-        default:
-            throw new RuntimeException("Unknown color blindness deficiency");
-        }
-    }
+	public static ColorDeficiencySimulator createSimulator(
+			Deficiency colorBlindness) {
+		switch (colorBlindness) {
+		case PROTANOPE:
+			return new ColorDeficiencySimulator(Deficiency.PROTANOPE);
+		case DEUTERANOPE:
+			return new ColorDeficiencySimulator(Deficiency.DEUTERANOPE);
+		case TRITANOPE:
+			return new ColorDeficiencySimulator(Deficiency.TRITANOPE);
+		case ACHROMATOPE:
+			return new ColorDeficiencySimulator(Deficiency.ACHROMATOPE);
+		default:
+			throw new RuntimeException("Unknown color blindness deficiency");
+		}
+	}
 
-    protected int[] precalcAchromatopeColorMap(float amount) {
-        int[] colorMap = new int[256 * 256 * 256];
-        for (int color = 0; color < colorMap.length; ++color) {
-            int startRed = (color & 0x00FF0000) >> 16;
-            int startGreen = (color & 0x0000FF00) >> 8;
-            int startBlue = (color & 0x000000FF);
+	protected int[] precalcAchromatopeColorMap(float amount) {
+		int[] colorMap = new int[256 * 256 * 256];
 
-            // Remove gamma correction using fast lookup table.
-            float linRed = removeGammaCorrectionLUT[startRed];
-            float linGreen = removeGammaCorrectionLUT[startGreen];
-            float linBlue = removeGammaCorrectionLUT[startBlue];
+		for (int color = 0; color < colorMap.length; ++color) {
+			int startRed = (color & 0x00FF0000) >> 16;
+			int startGreen = (color & 0x0000FF00) >> 8;
+			int startBlue = (color & 0x000000FF);
 
-            int simRed = applyGammaCorrectionLUT[(int) (ColorUtilities
-                    .clip(linRed * ColorUtilities.achromatopeSim.v1 + linGreen
-                            * ColorUtilities.achromatopeSim.v2 + linBlue
-                            * ColorUtilities.achromatopeSim.v3) * (MAX_ENCODED_VALUE - 1))];
-            int simGreen = simRed;
-            int simBlue = simRed;
+			// Remove gamma correction using fast lookup table.
+			float linRed = removeGammaCorrectionLUT[startRed];
+			float linGreen = removeGammaCorrectionLUT[startGreen];
+			float linBlue = removeGammaCorrectionLUT[startBlue];
 
-            // Anomylize colors
-            int finalRed = (int) (startRed * (1 - amount) + simRed * amount);
-            int finalGreen = (int) (startGreen * (1 - amount) + simGreen
-                    * amount);
-            int finalBlue = (int) (startBlue * (1 - amount) + simBlue * amount);
+			int simRed = applyGammaCorrectionLUT[(int) (ColorUtilities
+					.clip(linRed * ColorUtilities.achromatopeSim.v1 + linGreen
+							* ColorUtilities.achromatopeSim.v2 + linBlue
+							* ColorUtilities.achromatopeSim.v3) * (MAX_ENCODED_VALUE - 1))];
+			int simGreen = simRed;
+			int simBlue = simRed;
 
-            colorMap[color] = (finalRed << 16) | (finalGreen << 8) | finalBlue;
-        }
+			// Anomylize colors
+			int finalRed = (int) (startRed * (1 - amount) + simRed * amount);
+			int finalGreen = (int) (startGreen * (1 - amount) + simGreen
+					* amount);
+			int finalBlue = (int) (startBlue * (1 - amount) + simBlue * amount);
 
-        return colorMap;
-    }
+			colorMap[color] = 0xFF000000 | (finalRed << 16) | (finalGreen << 8)
+					| finalBlue;
+		}
 
-    protected int[] precalcDichromaticColorMap(Matrix sim, float amount) {
-        int[] colorMap = new int[256 * 256 * 256];
-        for (int color = 0; color < colorMap.length; ++color) {
-            int startRed = (color & 0x00FF0000) >> 16;
-            int startGreen = (color & 0x0000FF00) >> 8;
-            int startBlue = (color & 0x000000FF);
+		return colorMap;
+	}
 
-            // Remove gamma correction using fast lookup table.
-            float linRed = removeGammaCorrectionLUT[startRed];
-            float linGreen = removeGammaCorrectionLUT[startGreen];
-            float linBlue = removeGammaCorrectionLUT[startBlue];
+	protected int[] precalcDichromaticColorMap(Matrix sim, float amount) {
+		int[] colorMap = new int[256 * 256 * 256];
+		for (int color = 0; color < colorMap.length; ++color) {
+			int startRed = (color & 0x00FF0000) >> 16;
+			int startGreen = (color & 0x0000FF00) >> 8;
+			int startBlue = (color & 0x000000FF);
 
-            Vector lms = ColorUtilities.convertLinearRGB2LMS(new Vector(linRed,
-                    linGreen, linBlue));
-            Vector simRGB = ColorUtilities.convertLMS2LinearRGB(sim
-                    .rightMult(lms));
+			// Remove gamma correction using fast lookup table.
+			float linRed = removeGammaCorrectionLUT[startRed];
+			float linGreen = removeGammaCorrectionLUT[startGreen];
+			float linBlue = removeGammaCorrectionLUT[startBlue];
 
-            // Apply gamma correction using fast lookup table
-            int simRed = applyGammaCorrectionLUT[(int) (ColorUtilities
-                    .clip(simRGB.v1) * (MAX_ENCODED_VALUE - 1))];
-            int simGreen = applyGammaCorrectionLUT[(int) (ColorUtilities
-                    .clip(simRGB.v2) * (MAX_ENCODED_VALUE - 1))];
-            int simBlue = applyGammaCorrectionLUT[(int) (ColorUtilities
-                    .clip(simRGB.v3) * (MAX_ENCODED_VALUE - 1))];
+			Vector lms = ColorUtilities.convertLinearRGB2LMS(new Vector(linRed,
+					linGreen, linBlue));
+			Vector simRGB = ColorUtilities.convertLMS2LinearRGB(sim
+					.rightMult(lms));
 
-            // Anomylize colors
-            int finalRed = (int) (startRed * (1.0 - amount) + simRed * amount);
-            int finalGreen = (int) (startGreen * (1.0 - amount) + simGreen
-                    * amount);
-            int finalBlue = (int) (startBlue * (1.0 - amount) + simBlue
-                    * amount);
+			// Apply gamma correction using fast lookup table
+			int simRed = applyGammaCorrectionLUT[(int) (ColorUtilities
+					.clip(simRGB.v1) * (MAX_ENCODED_VALUE - 1))];
+			int simGreen = applyGammaCorrectionLUT[(int) (ColorUtilities
+					.clip(simRGB.v2) * (MAX_ENCODED_VALUE - 1))];
+			int simBlue = applyGammaCorrectionLUT[(int) (ColorUtilities
+					.clip(simRGB.v3) * (MAX_ENCODED_VALUE - 1))];
 
-            colorMap[color] = (finalRed << 16) | (finalGreen << 8) | finalBlue;
-        }
+			// Anomylize colors
+			int finalRed = (int) (startRed * (1.0 - amount) + simRed * amount);
+			int finalGreen = (int) (startGreen * (1.0 - amount) + simGreen
+					* amount);
+			int finalBlue = (int) (startBlue * (1.0 - amount) + simBlue
+					* amount);
 
-        return colorMap;
-    }
+			colorMap[color] = 0xFF000000 | (finalRed << 16) | (finalGreen << 8)
+					| finalBlue;
+		}
+
+		return colorMap;
+	}
 }
